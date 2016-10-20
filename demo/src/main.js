85c4bd8034f8b84a9c82d58766b80bc9f01654eb
David Baldwynn <shorerydr10@live.com>
2016-10-19 18:12:29 -0700

    fixed froala-editor dependency import problem

--- demo/src/main.js
+++ demo/src/main.js
@@ -5,9 +5,6 @@ import $ from "jquery";
 import froala from '../node_modules/froala-editor/js/froala_editor.pkgd.min';
 import "froala-editor/css/froala_editor.min.css";
 import "froala-editor/css/froala_style.min.css";
-
-//import "font-awesome/fonts/font-awesome-webfont.eot";
-//import "font-awesome/fonts/font-awesome-webfont.woff";
 import "font-awesome/css/font-awesome.css";
 
 // supports both of Vue 1.0 and Vue 2.0
