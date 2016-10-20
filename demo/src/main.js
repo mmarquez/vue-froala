59101ca54b0670593986a0b61dd764a14a5457ef
David Baldwynn <shorerydr10@live.com>
2016-10-19 17:01:02 -0700

    updated README

--- demo/src/main.js
+++ demo/src/main.js
@@ -6,10 +6,6 @@ var froala = require('../node_modules/froala-editor/js/froala_editor.pkgd.min');
 import "froala-editor/css/froala_editor.min.css";
 import "froala-editor/css/froala_style.min.css";
 
-console.log(froala);
-
-//$('body').froalaEditor();
-
 // supports both of Vue 1.0 and Vue 2.0
 import VueFroala from 'vue-froala'
 
